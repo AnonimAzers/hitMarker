205e75d44690096921cc631cdcfeacc4b2d447b1
AnonimAzers <62109114+AnonimAzers@users.noreply.github.com>
2021-03-30 02:23:11 +0300

    Update appFunc.h

--- SFPlugin/appFunc.h
+++ SFPlugin/appFunc.h
@@ -7,6 +7,7 @@ struct scriptSettings {
 	int randRange = 10;
 	bool status = false;
 };
+scriptSettings SS;
 
 struct GiveAndTakeDMG {
 	bool bGiveOrTake = false;
@@ -14,15 +15,15 @@ struct GiveAndTakeDMG {
 	UINT32 dWeaponID = 0, dBodypart = 0;
 	float damage_amount = 0;
 };
-scriptSettings SS;
+GiveAndTakeDMG GATDMG;
 
 void getCrossHairPos(int &crosshairPosX, int &crosshairPosY)
 {
 	int iHeight, iWidth;
 	SF->getGame()->getScreenResolution(&iWidth, &iHeight);
 	float chOff1 = *(float*)0xB6EC10, chOff2 = *(float*)0xB6EC14;
-	crosshairPosX = iWidth * chOff2; // Координаты прицела по оси Z
-	crosshairPosY = iHeight * chOff1; // Координаты прицела по оси Y
+	crosshairPosX = iWidth * chOff2; // ГЉГ®Г®Г°Г¤ГЁГ­Г ГІГ» ГЇГ°ГЁГ¶ГҐГ«Г  ГЇГ® Г®Г±ГЁ Z
+	crosshairPosY = iHeight * chOff1; // ГЉГ®Г®Г°Г¤ГЁГ­Г ГІГ» ГЇГ°ГЁГ¶ГҐГ«Г  ГЇГ® Г®Г±ГЁ Y
 }
 
 double getDistanceBetweenCoords3d(float x1, float y1, float z1, float x2, float y2, float z2) {
@@ -61,26 +62,26 @@ void saveSettingsInt(LPCSTR section, LPCSTR keyName, int lpString) {
 
 void CALLBACK changeDelay(std::string param) {
 	if (param.empty()) {
-		SF->getSAMP()->getChat()->AddChatMessage(D3DCOLOR_XRGB(0xFF, 0xFF, 0xFF), "[{ff6666}H{f2e9cd}Marker{ffffff}] Неверно указан параметр! /changedelay (millisec)");
+		SF->getSAMP()->getChat()->AddChatMessage(D3DCOLOR_XRGB(0xFF, 0xFF, 0xFF), "[{ff6666}H{f2e9cd}Marker{ffffff}] ГЌГҐГўГҐГ°Г­Г® ГіГЄГ Г§Г Г­ ГЇГ Г°Г Г¬ГҐГІГ°! /changedelay (millisec)");
 		return;
 	}
 	int i = std::stoi(param);
-	SF->getSAMP()->getChat()->AddChatMessage(D3DCOLOR_XRGB(0xFF, 0xFF, 0xFF), "[{ff6666}H{f2e9cd}Marker{ffffff}] Cкорость хитмаркера изменена на: {ff6666}%d", i);
+	SF->getSAMP()->getChat()->AddChatMessage(D3DCOLOR_XRGB(0xFF, 0xFF, 0xFF), "[{ff6666}H{f2e9cd}Marker{ffffff}] CГЄГ®Г°Г®Г±ГІГј ГµГЁГІГ¬Г Г°ГЄГҐГ°Г  ГЁГ§Г¬ГҐГ­ГҐГ­Г  Г­Г : {ff6666}%d", i);
 	SS.timeDelay = i;
 	saveSettingsInt("settings", "Delay", i);
 }
 
 void CALLBACK changeSize(std::string param) {
 	if (param.empty()) {
-		SF->getSAMP()->getChat()->AddChatMessage(D3DCOLOR_XRGB(0xFF, 0xFF, 0xFF), "[{ff6666}H{f2e9cd}Marker{ffffff}] Неверно указан параметр! /changesize (1 > inf)");
+		SF->getSAMP()->getChat()->AddChatMessage(D3DCOLOR_XRGB(0xFF, 0xFF, 0xFF), "[{ff6666}H{f2e9cd}Marker{ffffff}] ГЌГҐГўГҐГ°Г­Г® ГіГЄГ Г§Г Г­ ГЇГ Г°Г Г¬ГҐГІГ°! /changesize (1 > inf)");
 		return;
 	}
 	int i = std::stoi(param);
 	if (i < 1) {
-		SF->getSAMP()->getChat()->AddChatMessage(D3DCOLOR_XRGB(0xFF, 0xFF, 0xFF), "[{ff6666}H{f2e9cd}Marker{ffffff}] Неверно указан параметр! /changesize (1 > inf)");
+		SF->getSAMP()->getChat()->AddChatMessage(D3DCOLOR_XRGB(0xFF, 0xFF, 0xFF), "[{ff6666}H{f2e9cd}Marker{ffffff}] ГЌГҐГўГҐГ°Г­Г® ГіГЄГ Г§Г Г­ ГЇГ Г°Г Г¬ГҐГІГ°! /changesize (1 > inf)");
 		return;
 	}
-	SF->getSAMP()->getChat()->AddChatMessage(D3DCOLOR_XRGB(0xFF, 0xFF, 0xFF), "[{ff6666}H{f2e9cd}Marker{ffffff}] Размер хитмаркера изменен на: {ff6666}%d", i);
+	SF->getSAMP()->getChat()->AddChatMessage(D3DCOLOR_XRGB(0xFF, 0xFF, 0xFF), "[{ff6666}H{f2e9cd}Marker{ffffff}] ГђГ Г§Г¬ГҐГ° ГµГЁГІГ¬Г Г°ГЄГҐГ°Г  ГЁГ§Г¬ГҐГ­ГҐГ­ Г­Г : {ff6666}%d", i);
 	SS.markerSize = i;
 	saveSettingsInt("settings", "Size", i);
 
@@ -88,15 +89,15 @@ void CALLBACK changeSize(std::string param) {
 
 void CALLBACK changeRange(std::string param) {
 	if (param.empty()) {
-		SF->getSAMP()->getChat()->AddChatMessage(D3DCOLOR_XRGB(0xFF, 0xFF, 0xFF), "[{ff6666}H{f2e9cd}Marker{ffffff}] Неверно указан параметр! /changerange (1 > inf)");
+		SF->getSAMP()->getChat()->AddChatMessage(D3DCOLOR_XRGB(0xFF, 0xFF, 0xFF), "[{ff6666}H{f2e9cd}Marker{ffffff}] ГЌГҐГўГҐГ°Г­Г® ГіГЄГ Г§Г Г­ ГЇГ Г°Г Г¬ГҐГІГ°! /changerange (1 > inf)");
 		return;
 	}
 	int i = std::stoi(param);
 	if (i < 1) {
-		SF->getSAMP()->getChat()->AddChatMessage(D3DCOLOR_XRGB(0xFF, 0xFF, 0xFF), "[{ff6666}H{f2e9cd}Marker{ffffff}] Неверно указан параметр! /changerange (1 > inf)");
+		SF->getSAMP()->getChat()->AddChatMessage(D3DCOLOR_XRGB(0xFF, 0xFF, 0xFF), "[{ff6666}H{f2e9cd}Marker{ffffff}] ГЌГҐГўГҐГ°Г­Г® ГіГЄГ Г§Г Г­ ГЇГ Г°Г Г¬ГҐГІГ°! /changerange (1 > inf)");
 		return;
 	}
-	SF->getSAMP()->getChat()->AddChatMessage(D3DCOLOR_XRGB(0xFF, 0xFF, 0xFF), "[{ff6666}H{f2e9cd}Marker{ffffff}] Разброс хитмаркера изменен на: {ff6666}%d", i);
+	SF->getSAMP()->getChat()->AddChatMessage(D3DCOLOR_XRGB(0xFF, 0xFF, 0xFF), "[{ff6666}H{f2e9cd}Marker{ffffff}] ГђГ Г§ГЎГ°Г®Г± ГµГЁГІГ¬Г Г°ГЄГҐГ°Г  ГЁГ§Г¬ГҐГ­ГҐГ­ Г­Г : {ff6666}%d", i);
 	SS.randRange = i;
 	saveSettingsInt("settings", "Range", i);
 }
@@ -105,12 +106,12 @@ void CALLBACK changeStatus(std::string param) {
 	char buf[6];
 	if (SS.status) {
 		SS.status = false;
-		SF->getSAMP()->getChat()->AddChatMessage(D3DCOLOR_XRGB(0xFF, 0xFF, 0xFF), "[{ff6666}H{f2e9cd}Marker{ffffff}] Плагин {ff0000}выключен{ffffff}!");
+		SF->getSAMP()->getChat()->AddChatMessage(D3DCOLOR_XRGB(0xFF, 0xFF, 0xFF), "[{ff6666}H{f2e9cd}Marker{ffffff}] ГЏГ«Г ГЈГЁГ­ {ff0000}ГўГ»ГЄГ«ГѕГ·ГҐГ­{ffffff}!");
 		sprintf_s(buf, "false");
 	}
 	else {
 		SS.status = true;
-		SF->getSAMP()->getChat()->AddChatMessage(D3DCOLOR_XRGB(0xFF, 0xFF, 0xFF), "[{ff6666}H{f2e9cd}Marker{ffffff}] Плагин {00ff00}включен{ffffff}!");
+		SF->getSAMP()->getChat()->AddChatMessage(D3DCOLOR_XRGB(0xFF, 0xFF, 0xFF), "[{ff6666}H{f2e9cd}Marker{ffffff}] ГЏГ«Г ГЈГЁГ­ {00ff00}ГўГЄГ«ГѕГ·ГҐГ­{ffffff}!");
 		sprintf_s(buf, "true");
 	}
 	WritePrivateProfileStringA("settings", "Status", buf, filename);
